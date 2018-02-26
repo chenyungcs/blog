d457b135615634adffa2f66cd67c0650d82a66d9
yunchen <374035917@qq.com>
2018-02-26 17:40:13 +0800

    新增Todo demo
    - todo

--- 201702/lucy/client/src/app/index.js
+++ 201702/lucy/client/src/app/index.js
@@ -6,15 +6,85 @@
  */
 import React from 'react';
 import {Button} from '../components';
+import Todo from '../views/todo';
 
 type Props = {
     name: string
 }
+type ToggleType = {
+    isOn?: boolean
+}
+type ToggleState = {
+    isOn: true | false,
+    name: string
+}
 
+/**
+ * if test
+ * @param props
+ * @return {*}
+ * @constructor
+ */
+function WarningBanner (props) {
+    if (!props.warn) {
+        return null;
+    }
+
+    const style = {
+        background: 'yellow',
+        color: '#fff'
+    };
+
+    return <div style={style}>{props.warn}</div>
+}
+
+class Toggle extends React.Component<ToggleType, ToggleState> {
+    static defaultProps = {
+        isOn: true
+    };
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            isOn: props.isOn,
+            name: props.isOn ? '开启' : '关闭'
+        };
+    }
+
+    /**
+     * 切换
+     * @param e
+     */
+    handleClick (e) {
+        this.setState({
+            isOn: !this.state.isOn,
+            name: this.state.isOn ? '关闭' : '开启'
+        });
+    }
+
+    render () {
+        const {name, isOn} = this.state;
+        return (
+            <div>
+                <button onClick={this.handleClick.bind(this)}>{name}</button>
+            </div>
+        );
+    }
+}
+
+/**
+ * 入口
+ * @param props
+ * @return {*}
+ * @constructor
+ */
 export default function App (props: Props) {
+    const toggle = <Toggle/>
     return (
         <div>
-            <Button name={'保存'}/>
+            {toggle}
+            <Todo/>
             <h1>halo {props.name}</h1>
         </div>
     );
---
+++ 201702/lucy/client/src/views/todo/index.js
@@ -0,0 +1,138 @@
+/**
+ * @flow
+ * @author    : yunchen
+ * @createdAt : 26/02/2018
+ */
+import React from "react";
+import './style.less';
+
+type TodoType = {
+    id: number,
+    value: string
+}
+type TodoState = {
+    todoList: Array<TodoType>
+}
+type TodoItemType = {
+    list: TodoState
+}
+
+
+class Input extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    handleInput (e) {
+
+    }
+
+    render () {
+        return <input {...this.props} />
+    }
+}
+
+class TodoInput extends React.Component {
+    static defaultProps = {
+        value: '',
+        placeholder: 'todo now'
+    }
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            value: props.value
+        }
+    }
+
+    handleInput (e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    handleAdd () {
+        if (this.state.value) {
+            this.props.addTodo(this.state.value);
+            this.setState({
+                value: ''
+            });
+        }
+    }
+
+    render () {
+        return (
+            <section className="todo-search-wrapper">
+                {/*<i className="todo-search-icon iconfont icon-yigouxuan1"></i>*/}
+                <form onSubmit={event => event.preventDefault()}>
+                    <input className="todo-search-input"
+                           placeholder={this.props.placeholder}
+                           value={this.state.value} onChange={this.handleInput.bind(this)}/>
+
+                    <button onClick={this.handleAdd.bind(this)}>Add</button>
+                </form>
+            </section>
+        );
+    }
+}
+
+function TodoList (props: TodoItemType) {
+    const nodeList = props.list.map(item => {
+        return (
+            <li key={item.id}>
+                {/*<i className="iconfont icon-weigouxuan1"></i>*/}
+                {item.value}
+            </li>
+        );
+    });
+
+    return (
+        <section className="todo-list-wrapper">
+            <ul>
+                {nodeList}
+            </ul>
+        </section>
+    );
+}
+function TodoTool (props) {
+    return <section>{props.total}</section>;
+}
+
+export default class Todo extends React.Component<TodoType, TodoState> {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            todoList: []
+        };
+    }
+
+    addTodo (value) {
+        var list = this.state.todoList;
+        list.push({
+            value,
+            checked: false,
+            id: list.length + 1
+        });
+
+        this.setState(list);
+    }
+
+    render () {
+        return (
+            <section className={'todo'}>
+                <header>Todo</header>
+                <section>
+                    <TodoInput addTodo={this.addTodo.bind(this)}/>
+                    <TodoList list={this.state.todoList}/>
+                    <TodoTool total={this.state.todoList.length}/>
+                </section>
+                <footer>
+                    todo's code by yunchen
+                </footer>
+
+            </section>
+        )
+    }
+}
